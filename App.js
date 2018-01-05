0993d6c752389a4a98664665af058d5a4d00d87d
Djeison Selzlein <djeisonaguir@gmail.com>
2018-01-05 15:23:36 -0200

    refs#28 - created listItem component and more styling

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+import ListItem from './src/components/ListItem/ListItem';
+
 export default class App extends React.Component {
   state = {
     placeName: '',
@@ -26,7 +28,7 @@ export default class App extends React.Component {
 
   render() {
     const placesOutput = this.state.places.map((place, i) => (
-      <Text key={i}>{place}</Text>
+      <ListItem key={i} placeName={place} />
     ));
     return (
       <View style={styles.container}>
@@ -44,7 +46,7 @@ export default class App extends React.Component {
             onPress={this.placeSubmitHandler}
           />
         </View>
-        <View>
+        <View style={styles.listContainer}>
           {placesOutput}
         </View>
       </View>
@@ -71,5 +73,8 @@ const styles = StyleSheet.create({
   },
   placeButton: {
     width: '30%',
+  },
+  listContainer: {
+    width: "100%"
   }
 });
