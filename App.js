0179e3ea8e8d443ecf450e2e65331e28d46e8ba7
Djeison Selzlein <djeisonaguir@gmail.com>
2018-01-05 10:29:51 -0200

    refs#24 - styling text input with flexbox

--- App.js
+++ App.js
@@ -16,7 +16,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <TextInput 
-          style={{width: 300, borderColor: 'black', borderWidth: 1}}
+          style={{width: 300}}
+          placeholder="An awesome placeholder"
           value={this.state.placeName}
           onChangeText={this.placeNameChangedEvent} 
         />
@@ -28,8 +29,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 25,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
