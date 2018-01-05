4d9a50ea898e24c3ea96abf2a4210f3964589e11
Djeison Selzlein <djeisonaguir@gmail.com>
2018-01-05 10:42:25 -0200

    refs#26 - positioning textInput and button with flexbox

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
@@ -15,12 +15,19 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="An awesome placeholder"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedEvent} 
-        />
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={{width: 300}}
+            placeholder="An awesome placeholder"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedEvent} 
+            style={styles.placeInput}
+          />
+          <Button 
+            title="Add"
+            style={styles.placeButton}
+          />
+        </View>
       </View>
     );
   }
@@ -34,4 +41,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%',
+  },
+  placeButton: {
+    width: '30%',
+  }
 });
